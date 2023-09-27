92bf7b647cc08dc2b190fa0933c722caefde23fb
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-09-27 14:18:10 +0900

    Toolbar custom layout

--- app/src/main/java/com/example/mygallerypractice/FrameActivity.kt
+++ app/src/main/java/com/example/mygallerypractice/FrameActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import com.example.mygallerypractice.databinding.ActivityFrameBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -13,6 +14,13 @@ class FrameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.apply {
+            title = "나만의 앨범"
+            setSupportActionBar(this)
+
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val images = (intent.getStringArrayExtra("images") ?: emptyArray())
             .map{uriString -> FrameItem(Uri.parse(uriString))}
@@ -27,6 +35,22 @@ class FrameActivity : AppCompatActivity() {
             tab,position ->
             binding.viewPager.currentItem = tab.position
         }.attach()
+
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return when(item.itemId){
+           android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/example/mygallerypractice/MainActivity.kt
+++ app/src/main/java/com/example/mygallerypractice/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
@@ -30,6 +32,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.apply{
+            title= "사진 가져오기"
+            setSupportActionBar(this)
+        }
+
+
+
         binding.loadImageButton.setOnClickListener{
             checkPermission()
         }
@@ -41,6 +50,28 @@ class MainActivity : AppCompatActivity() {
         initRecyclerView()
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_add ->{
+                checkPermission()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+
+    }
+
+
 
     private fun navigateToFrameActivity(){
         val images = imageAdapter.currentList.filterIsInstance<ImageItems.Image>().map{it.uri.toString()}.toTypedArray()
